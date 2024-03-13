567ae9c343e7b52ad1f654a601d139b180df3ae1
Andy <huylv.dev@gmail.com>
2024-03-14 02:49:23 +0700

    add post

--- api/index.js
+++ api/index.js
@@ -7,8 +7,6 @@ const path = require('path');
 const axios = require('axios');
 const url = 'https://api.telegram.org/bot';
 
-// Create application/x-www-form-urlencoded parser
-// const urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.use(bodyParser.json());
 
 app.use(express.static('public'));
@@ -23,22 +21,23 @@ app.get('/', function (req, res) {
 app.post('/', async (req, res) => {
 
     console.log(req.body);
+    const sentMessage = req.body.message.text;
     let img = 'https://64.media.tumblr.com/702cb5b37ac53d0a5a4038546bf247a2/ad080d7fd0e814df-ee/s2048x3072/977ef6d312ca650b58b8e66a9282de87b41d7c09.jpg'
 
-    // axios.post(`${url}${process.env.BOT_TOKEN}/sendPhoto`,
-    //     {
-    //         chat_id: process.env.CHAT_ID,
-    //         photo: img
-    //     })
-    //     .then((response) => {
-    //         res.status(200).send(response);
-    //     }).catch((error) => {
-    //         res.send(error);
-    //     });
-
-    res.json(req.body);
-
-
+    if (sentMessage == 'ảnh' || sentMessage == 'Ảnh') {
+        axios.post(`${url}${process.env.BOT_TOKEN}/sendPhoto`,
+            {
+                chat_id: process.env.CHAT_ID,
+                photo: img
+            })
+            .then((response) => {
+                res.status(200).send(response);
+            }).catch((error) => {
+                res.send(error);
+            });
+    } else {
+        res.status(200).send({});
+    }
 });
 
 app.get('/about', function (req, res) {
